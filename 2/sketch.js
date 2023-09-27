5130243a459306e3c99d456fb0612aeaf9168d33
shuoshan <ss17564@nyu.edu>
2023-09-27 16:29:27 -0400

    time animation update

--- 2/sketch.js
+++ 2/sketch.js
@@ -1,11 +1,15 @@
 function setup() {
   createCanvas(windowWidth, windowHeight);
   fill(255);
+  print(millis());
+
 }
 
 
 function draw() {
-  background("blue"); 
-
-
+  background("green"); 
+  let secondsNow = second();
+  let myDiameter = map(secondsNow, 0, 59, 100, height);
+  ellipse(width / 2, height / 2, myDiameter, myDiameter);
+  
 }
